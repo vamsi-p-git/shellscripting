4e6c2fce3e2f1aa1dde02e3fc78cb4fb3f0dc628
vamsisaikrishna.pentela <vamsisaikrishna.pentela@gmail.com>
2026-04-19 21:52:52 +0530

    variables

---
+++ var_special.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+name1=$1
+name2=$2  #no spaces between value and =
+
+echo "$name1:: Hi $name2"
+echo "$name2:: Hello $name1"
+echo "$name1 :: what are you doing"
+echo "$name2 :: just chilling, what about you?"
+echo "$name1::getting bored!, lets meet"
