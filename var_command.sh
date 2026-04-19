cf2878b1cc0c4d9d620e0514024b47bacd0d9cd6
vamsisaikrishna.pentela <vamsisaikrishna.pentela@gmail.com>
2026-04-19 22:59:32 +0530

    var_specials

--- var_command.sh
+++ var_command.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-time_stamp=$(date)
+start_time=$(date +%s)
 
-echo "script executed time is $time_stamp"
+echo "script executed time is $start_time"
+sleep 10
+end_time=$(date +%s)
+total_time=$(($end_time - $start_time))
+
+echo "script executed time is $total_time"
---
+++ var_special_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All arguments passed: $@"
+echo "Number of variables passed : $@"
+echo "Present working directory : $PWD"
+echo "who is running the : $USER"
+echo "Home directory of the user : $HOME"
+echo "process id of the script :  $$"
+sleep 100 &
+echo "process id of the recently executed backgroud process : $!"
+echo "All arguments passed with * : $*"
