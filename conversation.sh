c93bde63fd847a37b5fc745c57f58d68a24d10de
vamsisaikrishna.pentela <vamsisaikrishna.pentela@gmail.com>
2026-04-19 21:31:42 +0530

    2nd script

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "vamsi:: Hi sathish"
+echo "sathish:: Hello vamsi"
+echo "vamsi :: what are you doing"
+echo "sathish :: just chilling, what about you?"
+echo "vamsi::getting bored!, lets meet"
