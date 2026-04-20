55215b18dc01cf1e034cda5f6072439b795fb75f
vamsisaikrishna.pentela <vamsisaikrishna.pentela@gmail.com>
2026-04-20 22:58:39 +0530

    var_arrays

---
+++ var_arrays.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fruits=("Banana" "Sapota" "Tomato")
+echo "Furits are : : ${fruits[@]}"
+echo "First fruit is : ${fruits[0]}"
+echo "Second fruit is : ${fruits[1]}"
+echo "Third fruit is : ${fruits[2]}"
